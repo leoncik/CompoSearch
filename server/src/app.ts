83b1dfa1dd8884cbc099651c03a662c260948557
Léonard Wojcik <leonardw@tutanota.com>
2023-07-08 10:21:34 +0200

    build: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express, { Express } from "express";
+import cors from 'cors'
 
 const app: Express = express();
 const port: number = 3000;
+
+// Middlewares
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
