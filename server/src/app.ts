ac47563964f29728559db86afcd8f98430ffd0c6
Léonard Wojcik <leonardw@tutanota.com>
2023-07-09 17:43:34 +0200

    refactor: add mocked data to server - Add mocked data to test communication between the client and server

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,67 @@ const port: number = 3000;
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json([{
+      "id": 1,
+      "first_name": "Con",
+      "last_name": "Gabotti"
+    }, {
+      "id": 2,
+      "first_name": "Edsel",
+      "last_name": "Woodberry"
+    }, {
+      "id": 3,
+      "first_name": "Olivero",
+      "last_name": "Steer"
+    }, {
+      "id": 4,
+      "first_name": "Florry",
+      "last_name": "Karpets"
+    }, {
+      "id": 5,
+      "first_name": "Moina",
+      "last_name": "De Winton"
+    }, {
+      "id": 6,
+      "first_name": "Maddie",
+      "last_name": "Chesley"
+    }, {
+      "id": 7,
+      "first_name": "Natividad",
+      "last_name": "Giorgetti"
+    }, {
+      "id": 8,
+      "first_name": "Travers",
+      "last_name": "Acklands"
+    }, {
+      "id": 9,
+      "first_name": "Larisa",
+      "last_name": "Colbrun"
+    }, {
+      "id": 10,
+      "first_name": "Orly",
+      "last_name": "Powdrill"
+    }, {
+      "id": 11,
+      "first_name": "Alyson",
+      "last_name": "Addicote"
+    }, {
+      "id": 12,
+      "first_name": "Melisse",
+      "last_name": "Polack"
+    }, {
+      "id": 13,
+      "first_name": "Georgena",
+      "last_name": "Bolmann"
+    }, {
+      "id": 14,
+      "first_name": "Lou",
+      "last_name": "Howle"
+    }, {
+      "id": 15,
+      "first_name": "Fawn",
+      "last_name": "Lates"
+    }])
   })
 
 app.listen(port, () => {
