0eb35e6f1e8c1d372443229afbf9b0c1f5faabe9
Léonard Wojcik <leonardw@tutanota.com>
2023-07-19 22:04:55 +0200

    refactor: delete unused mocks service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { MocksServiceService } from './mocks-service.service'
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'client';
-  mockSubscription$!: Subscription;
-
-  constructor(private mocksService: MocksServiceService) {}
-
-  ngOnInit(): void {
-    this.mockSubscription$ = this.mocksService.getMockedData().subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.mockSubscription$.unsubscribe();    
-  }
-}
+export class AppComponent {}
