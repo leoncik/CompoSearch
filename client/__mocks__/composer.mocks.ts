cf6208a7f1cd32c9e2f6076ab6b020c3dbc18f2a
Léonard Wojcik <leonardw@tutanota.com>
2023-08-26 11:15:05 +0200

    test: mock the API call

---
+++ client/__mocks__/composer.mocks.ts
@@ -0,0 +1,38 @@
+export const mockedJsonComposers = [
+    {
+        id: 1,
+        last_name: 'Bach',
+        first_name: 'Johann Sebastian',
+        avatar_file_name: '/avatars/Johann_Sebastian_Bach.jpg',
+    },
+    {
+        id: 2,
+        last_name: 'Mozart',
+        first_name: 'Wolfgang Amadeus',
+        avatar_file_name: '/avatars/default-avatar.png',
+    },
+    {
+        id: 3,
+        last_name: 'Beethoven',
+        first_name: 'Ludwig van',
+        avatar_file_name: '/avatars/Beethoven.jpg',
+    },
+    {
+        id: 4,
+        last_name: 'Chopin',
+        first_name: 'Frédéric',
+        avatar_file_name: '/avatars/Chopin-Delacroix.jpg',
+    },
+    {
+        id: 5,
+        last_name: 'Debussy',
+        first_name: 'Claude',
+        avatar_file_name: '/avatars/default-avatar.png',
+    },
+    {
+        id: 6,
+        last_name: 'Handel',
+        first_name: 'George Frideric',
+        avatar_file_name: '/avatars/default-avatar.png',
+    },
+];
--- client/e2e/home.spec.ts
+++ client/e2e/home.spec.ts
@@ -1,11 +1,30 @@
 import { test, expect } from '@playwright/test';
+import { mockedJsonComposers } from '__mocks__/composer.mocks';
 
 test.beforeEach(async ({ page }) => {
+    // Mock the api call before navigating
+    await page.route(
+        'http://localhost:3000/composers/?name=',
+        async (route) => {
+            const json = mockedJsonComposers;
+            await route.fulfill({ json });
+        }
+    );
     await page.goto('http://localhost:4200/');
 });
 
 test.describe('Page layout', () => {
     test('should have a title', async ({ page }) => {
         await expect(page).toHaveTitle(/CompoSearch/);
+    });
+
+    test('should display the composers names from the API', async ({
+        page,
+    }) => {
+        await expect(page.getByRole('table')).toBeVisible();
+        mockedJsonComposers.map(async (composer) => {
+            await expect(page.getByText(composer.first_name)).toBeVisible();
+            await expect(page.getByText(composer.last_name)).toBeVisible();
+        });
     });
 });
